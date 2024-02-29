d51f76eecf938e7b4a881838305a9c1316310524
Eric Joseph <ericjoseph2003@gmail.com>
2024-02-29 23:05:25 +1000

    first commit

---
+++ Week 1/Abtraction_inheritance/src/models/Animals.java
@@ -0,0 +1,10 @@
+package models;
+
+abstract class Animals {
+//    Abstract method (does not have body)
+    public abstract void animalSound();
+    public void sleep()
+    {
+        System.out.println("Zzz");
+    }
+}
---
+++ Week 1/Abtraction_inheritance/src/models/Pig.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Pig extends Animals {
+//    The sound a pig makes is given here
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
